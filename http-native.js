b1402665ffcf49ea2df2e90a18f4cb73fd76df7b
Stanley Andrade <45856878+StanleyAndrade@users.noreply.github.com>
2023-05-17 13:34:06 -0300

    Create http-native.js

---
+++ http-native.js
@@ -0,0 +1,18 @@
+//Servidor Nativo
+//importando o http
+const http = require('http')
+
+//definindo uma porta pro servidor estar disponível
+const port = 8080;
+
+//criando o servidor
+//req é request(requisição) / res é response(resposta)
+const server = http.createServer((req, res) => {
+    if (req == './home') { //requisição
+        res.writeHead(200, {"Content-Type": "text/html"}) //fala qual tipo de conteúdo estamos enviando p/renderizar; (statuscode)
+        res.end("<h1>Oi</h1>") //resposta
+    }
+})
+
+//colocando servidor online
+server.listen(port, () => console.log(`Rodando na porta ${port}`))
