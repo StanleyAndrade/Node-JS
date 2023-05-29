cd837a076df71f7b1bce89a56507797db1fb45a3
Stanley Andrade <45856878+StanleyAndrade@users.noreply.github.com>
2023-05-29 01:09:02 -0300

    Update http-expressjs.js

--- http-expressjs.js
+++ http-expressjs.js
@@ -1,12 +1,28 @@
+//Lembre de importar o express na pasta index: require('./modules/express')
+//Importando o express
 const express = require('express')
 
+//Inicializando o Express e colocando numa variável
 const app = express()
 
-//pega dados
+//GET - HTTP
 app.get("/home", (req, res) => {
     res.status(200).send("<h1>Oi<h1>")
 })
 
+//GET - HTTP
+app.get("/users", (req, res) => {
+    const users = [
+        {
+            name: "Stanley Andrade",
+            email: 'stanleyandrade95@gmail.com',
+        }
+    ]
+    res.status(200).json(users)
+})
+
 //definindo a porta
 const port = 8080
+
+//Função que será executada quando o servidor ficar Online
 app.listen(port, () => console.log(`Rorando com Express na porta ${port}!`))
