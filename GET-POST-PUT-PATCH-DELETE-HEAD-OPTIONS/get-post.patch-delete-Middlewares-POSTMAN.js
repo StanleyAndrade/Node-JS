88b7e4c3c455b01f9473dc185de4bf2214fdc8a6
Stanley Andrade <45856878+StanleyAndrade@users.noreply.github.com>
2023-06-04 00:24:42 -0300

    Update get-post.patch-delete-Middlewares-POSTMAN.js

--- GET-POST-PUT-PATCH-DELETE-HEAD-OPTIONS/get-post.patch-delete-Middlewares-POSTMAN.js
+++ GET-POST-PUT-PATCH-DELETE-HEAD-OPTIONS/get-post.patch-delete-Middlewares-POSTMAN.js
@@ -1,26 +1,83 @@
-//Lembre de importar o express na pasta index: require('./modules/express')
-//Importando o express
+//Importando o express + Lembre de importar na pasta index: require('./modules/express')
 const express = require('express')
+
+//Inicializando o Express e colocando na variavel router1
+const app = express()
+const router1 = express.Router()
 
 //importando user_model para usá-lo aqui no express (mas pode usar em qualquer lugar)
 const UserModel = require("../src/models/user_model")
-
-//Inicializando o Express e colocando numa variável
-const app = express()
+//const { default: mongoose } = require('mongoose')
 
 //sinalizando que vai receber JSON
-app.use(express.json())
+router1.use(express.json())
+
+//EJS - Configurando + mostrando a pasta e o arquivo
+app.set("view engine", "ejs")
+app.set("views", "src/views") 
+
+//enviando a view
+router1.get('/views/users', async (req, res) => {
+    const users = await UserModel.find({});
+    res.render("index", { users });
+})
 
 //Middlewares - função que é executada antes da requisição
-app.use((req, res, next) => {
+router1.use((req, res, next) => {
     console.log(`Request Type: ${req.method}`) //mostra se foi GET, POST, PATCH, PUT ou Delete
-    console.log(`Content Type: ${req.headers["content-type"]}`) //mostra o que é e seu tipo: application.json
+    console.log(`Content Type: ${req.headers["content-type"]}`) //mostra o que é e seu tipo: router1lication.json
     console.log(`Date: ${new Date()}`) //mostra dia e hora
     next()
 })
 
-//GET - Pegando os dados do database - TUDO
-app.get("/users", async (req, res) => {
+//Rotas
+router1.get('/', (req, res) => {
+    res.send('Sou a Home 1')
+})
+
+router1.get('/sobre', (req, res) => {
+    res.send('Sobre Nós')
+})
+
+//Post 2 TENTATIVA
+router1.post("/users", async (req, res) => {
+    try {
+        const user = await UserModel.create(req.body)
+        res.status(200).json(user)
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+})
+
+dessa forma dá pra colocar dentro de uma função
+const novo = mongoose.model('UserModel')
+
+new novo({
+    firstName: "Katharine",
+    lastName: "jorge",
+    email: "oi@gmail.com",
+    password: 123456789
+}).save()
+.then(() => {
+    console.log("Salvo com sucrsso")
+})
+.catch(() => {
+    console.log("Erro ao salvar")
+})
+
+//POST - enviando dados para o databse
+//Obs.: os dados enviados estão no Postman
+router1.post("/users", async (req, res) => {
+    try {
+        const user = await UserModel.create(req.body)
+        res.status(200).json(user)
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+})
+
+//GET - Pegando TODOS os dados do database
+router1.get("/users", async (req, res) => {
     try {
         const user = await UserModel.find({})
         res.status(200).json(users)
@@ -29,8 +86,8 @@ app.get("/users", async (req, res) => {
     }
 })
 
-//GET - pegando usuário por ID
-app.get("/users/:id", async (req, res) => {
+//GET - pegando dados por ID
+router1.get("/users/:id", async (req, res) => {
     try {
         const id = req.params.id;
         const user = await UserModel.findById(id)
@@ -40,19 +97,8 @@ app.get("/users/:id", async (req, res) => {
     }
 })
 
-//POST - enviando dados para o databse
-//Obs.: os dados enviados estão no Postman
-app.post("/users", async (req, res) => {
-    try {
-        const user = await UserModel.create(req.body)
-        res.status(200).json(user)
-    } catch (error) {
-        return res.status(500).send(error.message)
-    }
-})
-
-//PATCH - Atualiza partes e não tudo
-app.patch("/users/:id", async (req, res) => {
+//PATCH - Atualiza partes e não tudo dos dados
+router1.patch("/users/:id", async (req, res) => {
     try {
         const id = req.params.id
         const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true})
@@ -62,8 +108,8 @@ app.patch("/users/:id", async (req, res) => {
     }
 })
 
-//Delete - Apaga tudo
-app.delete("/users/:id", async (req, res) => {
+//Delete - Apagando dados
+router1.delete("/users/:id", async (req, res) => {
     try {
         const id = req.params.id
         const user = await UserModel.findByIdAndRemove(id)
@@ -73,8 +119,4 @@ app.delete("/users/:id", async (req, res) => {
     }
 })
 
-//definindo a porta
-const port = 8080
-
-//Função que será executada quando o servidor ficar Online
-app.listen(port, () => console.log(`Rorando com Express na porta ${port}!`))
+module.exports = router1
